6df2219ee282635236b78ce3f33ff82cdefeacab
Katha Vault <kathavault@gmail.com>
2025-05-03 06:57:07 +0000

    Initial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDT4-R0H8uydAqzrTsHfMKLXQ59p7u67Ho",
+  authDomain: "katha-vault-novel.firebaseapp.com",
+  databaseURL: "https://katha-vault-novel-default-rtdb.firebaseio.com",
+  projectId: "katha-vault-novel",
+  storageBucket: "katha-vault-novel.firebasestorage.app",
+  messagingSenderId: "1050410197456",
+  appId: "1:1050410197456:web:3add67c05dac9fe2c419d5"
+};
